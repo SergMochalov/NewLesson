124e3576e58317b172579df88f53c99840be8f81
SergMochalov <Serginho19860207@yandex.ru>
2025-01-07 17:17:31 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,7 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(s1) - len(s2) for s1, s2 in zip(first, second) if not len(s1) == len(s2))
+second_result = (len(first[i]) == len(second[i]) for i in range(0, len(first)))
+print(list(first_result))
+print(list(second_result))
